b132a44e623f6100752654c39fedf0e3d162e9b9
Sophie <sophiebrown438@gmail.com>
2020-08-26 15:34:20 +0100

    Pass test for returning one integer

--- __tests__/paperScanner.test.js
+++ __tests__/paperScanner.test.js
@@ -5,4 +5,8 @@ describe('#PaperScanner', () => {
   test('it should return 1 as string and int', () => {
     expect(paperScanner.calculate("1")).toEqual(["1", 1])
   })
+
+  test('it should return 1 as string and int', () => {
+    expect(paperScanner.calculate("2")).toEqual(["2", 2])
+  })
 })
--- src/paperScanner.js
+++ src/paperScanner.js
@@ -1,5 +1,11 @@
 class PaperScanner {
 
+  calculate(score){
+    let result = []
+    result.push(score, parseInt(score, 10))
+    return result
+  }
+
 }
 
 module.exports = PaperScanner
